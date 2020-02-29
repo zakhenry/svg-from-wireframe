d0343290e9018b80c7bce878fd263b7bdcee7711
Zak Henry <zak@cloudnc.com>
2020-02-29 19:26:26 +0000

    feat(Wireframe): Make basic loading of wireframe into svg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,26 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, NgZone, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  NgZone,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import {
   ArcRotateCamera,
   Camera,
   Engine,
   HemisphericLight,
   Matrix,
-  MeshBuilder,
   Scene,
   Vector3,
+  VertexBuffer,
 } from '@babylonjs/core';
 import { Observable, Subject } from 'rxjs';
-import { map, switchAll } from 'rxjs/operators';
+import { map, shareReplay, startWith, switchAll, tap } from 'rxjs/operators';
+import { createMesh, MeshAssetData } from './load-mesh';
 
 @Component({
   selector: 'app-root',
@@ -22,12 +32,13 @@ export class AppComponent implements AfterViewInit {
   title = 'wireframe-svg-maker';
 
   @ViewChild('canvas') canvas: ElementRef;
+  @ViewChild('svg') svg: ElementRef;
 
-  constructor(private zone: NgZone) {
+  constructor(private zone: NgZone, private http: HttpClient, private renderer: Renderer2) {
   }
 
-  private pointerPos$$ = new Subject();
-  public pointerPos$ = this.pointerPos$$.pipe(switchAll());
+  private lines$$ = new Subject();
+  public lines$ = this.lines$$.pipe(switchAll());
 
   public ngAfterViewInit() {
 
@@ -59,7 +70,7 @@ export class AppComponent implements AfterViewInit {
     camera.wheelPrecision = 0.5;
     camera.attachControl(canvasElement, false, true, 1);
     camera.mode = Camera.ORTHOGRAPHIC_CAMERA;
-    (camera.inputs.attached.pointers as any).buttons = [1, 2];
+    // (camera.inputs.attached.pointers as any).buttons = [1, 2];
 
     camera.minZ = -10000;
     camera.maxZ = 10000;
@@ -98,37 +109,69 @@ export class AppComponent implements AfterViewInit {
     // Default intensity is 1. Let's dim the light a small amount
     light.intensity = 0.7;
 
-    // Our built-in 'sphere' shape.
-    const sphere = MeshBuilder.CreateSphere('sphere', { diameter: 20, segments: 32 }, scene);
-
-    // Move the sphere upward 1/2 its height
-    sphere.position.y = 10;
-
-    // Our built-in 'ground' shape.
-    MeshBuilder.CreateGround('ground', { width: 60, height: 60 }, scene);
-
-    const point = MeshBuilder.CreateSphere('sphere', { diameter: 5, segments: 8 }, scene);
-    point.position = new Vector3(200, 40, 50);
-
     const render$ = new Observable(observer => {
-      const ref = scene.onAfterRenderObservable.add(() => observer.next());
-      return () => scene.onAfterRenderObservable.remove(ref);
-    });
-
-    this.pointerPos$$.next(render$.pipe(map(() => {
-      const worldMatrix = Matrix.Identity() || point.getWorldMatrix();
-      const transformMatrix = scene.getTransformMatrix();
-      const viewport = scene.activeCamera.viewport;
-      const coordinates = Vector3.Project(point.position, worldMatrix, transformMatrix, viewport);
-
-      return {
-        x: canvasElement.width * coordinates.x,
-        y: canvasElement.height * coordinates.y,
-      };
-    })));
+      const ref = camera.onViewMatrixChangedObservable.add(() => observer.next());
+      return () => camera.onViewMatrixChangedObservable.remove(ref);
+    }).pipe(startWith(null));
 
     this.zone.runOutsideAngular(() => engine.runRenderLoop(() => scene.render()));
 
+    this.http.get<MeshAssetData>('/assets/mesh.json').pipe(tap((res) => {
+
+      const mesh = createMesh('foo', scene, res);
+
+      const vertices = mesh.getVerticesData(VertexBuffer.PositionKind);
+
+      const lines: [Vector3, Vector3][] = [];
+
+      for (let i = 0; i < vertices.length; i += 6) {
+
+        lines.push([
+          new Vector3(vertices[i], vertices[i + 1], vertices[i + 2]),
+          new Vector3(vertices[i + 3], vertices[i + 4], vertices[i + 5]),
+        ]);
+
+      }
+
+      this.lines$$.next(render$.pipe(map(() => {
+        const worldMatrix = Matrix.Identity();
+        const transformMatrix = scene.getTransformMatrix();
+        const viewport = scene.activeCamera.viewport;
+
+        return lines.map((line) => {
+
+          return line.map(v => {
+
+            const coordinates = Vector3.Project(v, worldMatrix, transformMatrix, viewport);
+            return {
+              x: canvasElement.width * coordinates.x,
+              y: canvasElement.height * coordinates.y,
+            };
+          });
+
+        });
+
+      })));
+
+    })).subscribe();
+
   }
 
+  public saveSvg() {
+    console.log({svg: this.svg.nativeElement});
+
+    const blob = new Blob(
+      [ this.svg.nativeElement.outerHTML ],
+      {
+        type : "text/plain;charset=utf-8"
+      }
+    );
+
+
+    const link = this.renderer.createElement('a');
+    link.style.display = 'none';
+    link.href = URL.createObjectURL( blob );
+    link.download = 'wireframe.svg';
+    link.dispatchEvent(new MouseEvent(`click`, { bubbles: true, cancelable: true, view: window }));
+  }
 }
---
+++ src/app/load-mesh.ts
@@ -0,0 +1,86 @@
+import { LinesMesh, Mesh, Scene, VertexData } from '@babylonjs/core';
+
+type FloatArray = number[] | Float32Array;
+type IndicesArray = number[] | Int32Array | Uint32Array | Uint16Array;
+
+
+enum GeometryType {
+  UNKNOWN = 0,
+  POINTS = 1,
+  LINES = 2,
+  LINE_STRIP = 3,
+  TRIANGLES = 4,
+  TRIANGLE_STRIP = 5,
+  TRIANGLE_FAN = 6
+}
+
+export interface MeshAssetData {
+  positions: FloatArray | null;
+  normals: FloatArray | null;
+  indices: IndicesArray | null;
+  geomType: GeometryType;
+}
+
+function generateLineIndices(positions: ArrayLike<number>): Int32Array {
+  const positionsLength = positions.length / 3;
+  const indices = new Int32Array(positionsLength);
+
+  for (let i = 0; i < positionsLength; i++) {
+    indices[i] = i;
+  }
+  return indices;
+}
+
+function generateLineStripIndices(positions: ArrayLike<number>): Int32Array {
+  const positionsLength = positions.length / 3;
+  const indices = new Int32Array(Math.max(positionsLength * 2 - 2, 0));
+
+  for (let i = positionsLength + 1; i > 0; i--) {
+    indices[i * 2 - 1] = i;
+    indices[i * 2] = i;
+  }
+  return indices;
+}
+
+
+export function createMesh(name: string, scene: Scene, meshData: MeshAssetData): Mesh {
+  const { positions, normals, indices, geomType } = meshData;
+
+  const vertexData = new VertexData();
+  vertexData.positions = positions;
+
+  let mesh: Mesh;
+
+  switch (geomType) {
+    case GeometryType.TRIANGLES: {
+      vertexData.indices = indices;
+      vertexData.normals = normals;
+
+      mesh = new Mesh(name, scene);
+      break;
+    }
+    case GeometryType.LINES: {
+      vertexData.indices = generateLineIndices(positions);
+      mesh = new LinesMesh(name, scene);
+      break;
+    }
+    case GeometryType.LINE_STRIP: {
+      vertexData.indices = generateLineStripIndices(positions);
+      mesh = new LinesMesh(name, scene);
+      break;
+    }
+    case GeometryType.POINTS: {
+      mesh = new Mesh(name, scene);
+      break;
+    }
+    case GeometryType.UNKNOWN:
+    default: {
+      throw new Error('when loading meshes');
+    }
+  }
+
+  vertexData.applyToMesh(mesh);
+  mesh._unIndexed = !indices || indices.length === 0;
+
+  return mesh;
+}
